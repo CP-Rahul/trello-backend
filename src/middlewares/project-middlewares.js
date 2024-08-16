66b64ef597700e66ea70efe46913565641afe46f
CP-Rahul <rahulcp1401@gmail.com>
2024-08-16 18:34:59 +0530

    Fixed middleware response

--- src/middlewares/project-middlewares.js
+++ src/middlewares/project-middlewares.js
@@ -3,13 +3,13 @@ const AppError = require('../utils/error/app-error');
 
 function validateCreateRequest(req, res, next) {
     if(!req.body.name) {
-        ErrorResponse.message = new AppError('name is not found in the request body', 400);
+        ErrorResponse.error = new AppError('name is not found in the request body', 400);
         return res
                 .status(400)
                 .json(ErrorResponse)
     }
     if(!req.body.description) {
-        ErrorResponse.message = new AppError('descriptionis not found in the request body', 400);
+        ErrorResponse.error = new AppError('descriptionis not found in the request body', 400);
         return res
                 .status(400)
                 .json(ErrorResponse)
--- src/middlewares/task-middlewares.js
+++ src/middlewares/task-middlewares.js
@@ -3,37 +3,37 @@ const AppError = require('../utils/error/app-error');
 
 function validateCreateRequest(req, res, next) {
     if(!req.body.name) {
-        ErrorResponse.message = new AppError('name is not found in the request body', 400);
+        ErrorResponse.error = new AppError('name is not found in the request body', 400);
         return res
                 .status(400)
                 .json(ErrorResponse)
     }
     if(!req.body.description) {
-        ErrorResponse.message = new AppError('description is not found in the request body', 400);
+        ErrorResponse.error = new AppError('description is not found in the request body', 400);
         return res
                 .status(400)
                 .json(ErrorResponse)
     }
     if(!req.body.tags) {
-        ErrorResponse.message = new AppError('tags is not found in the request body', 400);
+        ErrorResponse.error = new AppError('tags is not found in the request body', 400);
         return res
                 .status(400)
                 .json(ErrorResponse)
     }
     if(!req.body.dueDate) {
-        ErrorResponse.message = new AppError('duedate is not found in the request body', 400);
+        ErrorResponse.error = new AppError('duedate is not found in the request body', 400);
         return res
                 .status(400)
                 .json(ErrorResponse)
     }
     if(!req.body.assignedUserId) {
-        ErrorResponse.message = new AppError('assignedUserId is not found in the request body', 400);
+        ErrorResponse.error = new AppError('assignedUserId is not found in the request body', 400);
         return res
                 .status(400)
                 .json(ErrorResponse)
     }
     if(!req.body.projectId) {
-        ErrorResponse.message = new AppError('projectId is not found in the request body', 400);
+        ErrorResponse.error = new AppError('projectId is not found in the request body', 400);
         return res
                 .status(400)
                 .json(ErrorResponse)
