8197973fb5351615d9801700c5d3a6114569990f
CP-Rahul <rahulcp1401@gmail.com>
2024-08-16 18:59:55 +0530

    Fixed bug in validateCreateRequest middleware response

--- src/middlewares/user-middlewares.js
+++ src/middlewares/user-middlewares.js
@@ -4,19 +4,19 @@ const { UserService } = require('../services');
 
 function validateCreateRequest(req, res, next) {
     if(!req.body.name) {
-        ErrorResponse.message = new AppError('name is not found in the request body', 400);
+        ErrorResponse.error = new AppError('name is not found in the request body', 400);
         return res
                 .status(400)
                 .json(ErrorResponse)
     }
     if(!req.body.email) {
-        ErrorResponse.message = new AppError('email is not found in the request body', 400);
+        ErrorResponse.error = new AppError('email is not found in the request body', 400);
         return res
                 .status(400)
                 .json(ErrorResponse)
     }
     if(!req.body.password) {
-        ErrorResponse.message = new AppError('password is not found in the request body', 400);
+        ErrorResponse.error = new AppError('password is not found in the request body', 400);
         return res
                 .status(400)
                 .json(ErrorResponse)
